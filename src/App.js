479b2f64ec3665a32aeca28c477f34dda1fc89d7
Raian-Siddique <raian0197@gmail.com>
2020-12-16 03:10:32 +0600

    fetch data from jason placeholder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [nayoks, setNayoks] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setNayoks(data));
+    return () => {};
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {nayoks.map((nk) => (
+        <Nayok name={nk.name} key={nk.id} age={nk.age}></Nayok>
+      ))}
+    </div>
+  );
+}
+function Nayok(props) {
+  const nayokStyles = {
+    border: "2px solid purple",
+    margin: "20 px",
+  };
+
+  return (
+    <div style={nayokStyles}>
+      <h2> I am Nayok:{props.name} </h2>
+      <h3>I have done 20 movies in {props.age || 30} years</h3>
     </div>
   );
 }
